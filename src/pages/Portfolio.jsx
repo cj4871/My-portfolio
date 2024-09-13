ca09d5862952b21a058d7377cfa1985dd4c11945
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-09-12 20:06:09 -0500

    added portfolio function

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+export default function Porfolio(){
+    return(
+        <>
+        <h2>Project:</h2>
+        <p>{data.title}</p>
+        <h2>Description:</h2>
+        <p>{data.desc}</p>
+        <img src={data.imgUrl} alt="" to=""/>
+
+        {/* finish this later */}
+        </>
+    )
+}
